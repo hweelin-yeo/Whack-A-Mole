a17f4342b0ef97d175c443d688172696c20eef1f
hweelin-yeo <yeohweelin_97@hotmail.com>
2018-05-17 12:49:08 -0400

    three test files

---
+++ led_test.cpp
@@ -0,0 +1,17 @@
+// This file tests for the LED panel in the following ways:
+// 		- checks if LED panel initialises correctly
+//		- checks for draw and clear functions
+// 		- checks if numbers are drawn correctly
+// 		- checks if moles are drawn correctly at the right positions
+// 
+
+#include "mbed.h"
+#include "RGBmatrixPanel.h" // Hardware-specific library
+#include "game.h"
+#include <cstdlib>
+#include "controller.h"
+#include "SNESController.h"
+
+int main() {
+
+}
